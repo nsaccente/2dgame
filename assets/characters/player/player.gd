8787443a7f2c68d276f95b2d0c8ba5c0d76df135
nsaccente <32527006+nsaccente@users.noreply.github.com>
2024-06-19 15:35:41 -0400

    Add basic player movement

---
+++ assets/characters/player/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed = 20000  # speed in pixels/sec
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed * delta
+	
+	print(
+		get_global_mouse_position()
+	)
+
+	
+	move_and_slide()
