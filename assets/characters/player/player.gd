aee1905dcccd9c70f2f4b31f6d212935fdb4f8d3
nsaccente <32527006+nsaccente@users.noreply.github.com>
2024-06-21 14:18:52 -0400

    Experimenting with player animation

--- assets/characters/player/player.gd
+++ assets/characters/player/player.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
-var speed = 20000  # speed in pixels/sec
+var SPEED = 10000  # speed in pixels/sec
+@onready var state_machine = $AnimationTree["parameters/playback"]
+@onready var sprite_2d = $Sprite2D
 
 func _physics_process(delta):
+	var current = state_machine.get_current_node()
+	print(current)
 	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed * delta
+	velocity = direction * SPEED * delta
 	
-	print(
-		get_global_mouse_position()
-	)
-
+	# True if the cursor is to the left of the player
+	var look_left = sprite_2d.get_local_mouse_position()[0] < 0
+	sprite_2d.flip_h = look_left
+	
+	if velocity.length() > 0:
+		state_machine.travel("run")
+	else:
+		state_machine.travel("idle")
 	
 	move_and_slide()
+#
