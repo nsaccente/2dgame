9f782052646f0c6fc2fd581e0baa3c6b54ea472e
nsaccente <nsaccente@proton.me>
2024-06-24 22:04:04 -0400

    Clean up player script

--- assets/characters/player/player.gd
+++ assets/characters/player/player.gd
@@ -1,93 +1,74 @@
 extends CharacterBody2D
 
-var MOVEMENT_SPEED: int = 300  # speed in pixels/sec
-const ANIMATION_DIRECTIONS: int = 8
+## Base player movement speed.
+const BASE_MOVEMENT_SPEED: int = 300
 
-const INPUT_LEFT: String = "left"
-const INPUT_RIGHT: String = "right"
-const INPUT_UP:	String = "up"
-const INPUT_DOWN: String = "down"
+## Set to number of sprite orientations; make sure to label your animations 
+## from 0 through (ANIMATION_DIRECTIONS-1), starting at 3:00
+## https://kidscancode.org/godot_recipes/4.x/2d/8_direction/
+const ANIMATION_DIRECTIONS: int = 2
+
+## Input configuration, update values with InputMap action names
+const INPUT: Dictionary = {
+	"move_left": "left",
+	"move_right": "right",
+	"move_up": "up",
+	"move_down": "down",
+}
+
+# Player state variables; no need to edit
+var movement_speed: int = BASE_MOVEMENT_SPEED  # speed in pixels/sec
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var state_machine: AnimationNodeStateMachinePlayback = (
 	animation_tree["parameters/StateMachine/playback"]
 )
 @onready var sprite_2d: Sprite2D = $Sprite2D
-var direction_parser = parse_direction
 	
-func parse_direction(M: Vector2) -> int:
-	var angle = snappedf(M.angle(), PI/(ANIMATION_DIRECTIONS/2)) / (PI/(ANIMATION_DIRECTIONS/2))
-	angle = wrapi(int(angle), 0, ANIMATION_DIRECTIONS)
-	
-	return angle
-
-func type(thing) -> String:
-	return type_string(
-		typeof(	
-			thing
-		)
+func parse_direction(M: Vector2, num_directions=ANIMATION_DIRECTIONS) -> int:
+	var angle = (
+		snappedf(
+			M.angle(), 
+			PI/(num_directions/2),
+		) 
+		/ (PI/(num_directions/2))
 	)
-
-func die(
-	velocity: Vector2, 
-	mouse_position: Vector2, 
-	current_anim: StringName,
-):
-	pass
-	
-func hurt(
-	velocity: Vector2, 
-	mouse_position: Vector2, 
-	current_anim: StringName,
-):
-	pass
+	return wrapi(int(angle), 0, num_directions)
 	
 func idle(
 	velocity: Vector2, 
 	mouse_position: Vector2, 
 	current_anim: StringName,
-):
-	#var mouse_angle: float = mouse_position.angle()
-	#print(mouse_position)
-	print(
-		parse_direction(
-			mouse_position
-		)
-	)
-	#if mouse_angle 
-	#if mouse_position[0] >= 0:
-		#print(mouse_position[0])
-	#else:
-		#pass
-	
-	state_machine.travel("right_idle")
+) -> void:
+	state_machine.travel("idle_" + str(parse_direction(mouse_position)))
 	
 func run(
 	velocity: Vector2, 
 	mouse_position: Vector2, 
 	current_anim: StringName,
-):
-	state_machine.travel("right_run")
+) -> void:
+	#animation_tree.set("parameters/TimeScale/scale", 1)
+	state_machine.travel("run_" + str(parse_direction(mouse_position)))
 
 func _physics_process(delta):
 	var current_anim = state_machine.get_current_node()
 	var mouse_position: Vector2 = sprite_2d.get_local_mouse_position()
 	
-	#if current_anim in ["hurt", "die"]:
-		#return
-		
 	velocity = (
-		Input.get_vector(INPUT_LEFT, INPUT_RIGHT, INPUT_UP, INPUT_DOWN)
-		 * MOVEMENT_SPEED
+		Input.get_vector(
+			INPUT["move_left"], 
+			INPUT["move_right"], 
+			INPUT["move_up"],
+			INPUT["move_down"],
+		) * movement_speed
 	)
-
-	animation_tree.set("parameters/TimeScale/scale", 1)
-	#if Input.is_action_just_pressed("attack"):
-		#state_machine.travel(attacks.pick_random())
-		#return
 
 	if velocity.length() > 0:
 		run(velocity, mouse_position, current_anim)
+	elif current_anim in ["hurt", "die"]:
+		return
+	elif Input.is_action_just_pressed("attack"):
+		return
 	else:
 		idle(velocity, mouse_position, current_anim)
 	move_and_slide()
